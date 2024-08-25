842a7251e3d6ff1062f06757cad62fbea7fb8446
maheshgamersop <141534592+maheshgamersop@users.noreply.github.com>
2024-08-25 14:04:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connect(); // Connecting to the database
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const server = http.createServer(app);
 
 // Initialize Socket.IO
