99801a03d3c4d37fcd8a157302b88b0da2127a97
maheshgamersop <141534592+maheshgamersop@users.noreply.github.com>
2024-10-06 18:01:57 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
@@ -16,18 +17,22 @@ const UserSchema = mongoose.Schema({
   },
   profile: {
     type: String,
-    default: 'https://i.postimg.cc/cCPwqGQz/discord-fake-avatar-decorations-1724674350651.gif'
+    default: 'https://i.postimg.cc/cCPwqGQz/discord-fake-avatar-decorations-1724674350651.gif',
   },
   phone_num: {
     type: String,
+  },
+}, { timestamps: true });
+
+UserSchema.pre('save', async function(next) {
+  // Check if the password field is modified
+  if (!this.isModified('password')) {
+    return next(); // Skip hashing if the password hasn't changed
   }
-}, {timestamps: true})
-UserSchema.pre('save', async function(next){
-  if(!this.isModified){
-    next()
-  }
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-})
-const User = mongoose.model('User', UserSchema)
-  export default User
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next(); // Call next to continue with the save process
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
